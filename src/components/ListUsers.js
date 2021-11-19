fe91e7f25662566b7228e5f4fe6f99f0cbdec7b8
mike-sincara <s11910062@student.unklab.ac.id>
2021-11-19 22:38:05 +0800

    Third Commit

--- src/components/ListUsers.js
+++ src/components/ListUsers.js
@@ -2,42 +2,46 @@ import React, { useState,useEffect } from 'react'
 
 
 export default function ListUsers() {
-    const [user, setApi] = useState([]);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState();
+    const [users, setApi] = useState([]);
+    //const [loading, setLoading] = useState(false);
+    //const [error, setError] = useState();
 
      useEffect(() => {
         fetch("https://reqres.in/api/users?page=2")
         .then((response) => response.json())
         .then((json)=> setApi(json))
-        .catch((err) => {
-            setError(err);
-          })
-          .finally(() => {
-            setLoading(false);
-          });
+       // .catch((err) => {
+       //     setError(err);
+        //  })
+        //  .finally(() => {
+       //     setLoading(false);
+        //  });
     }, [])
 
-    if (loading) {
-        return <p>Data is loading...</p>;
-      }
+  //  if (loading) {
+   //     return <p>Data is loading...</p>;
+   //   }
     
-    if (error || !Array.isArray(user)) {
-        return <p>There was an error loading your data!</p>;
-      }
+   // if (error || !Array.isArray(user)) {
+   //     return <p>There was an error loading your data!</p>;
+    //  }
     return (
         <div>
             <h3>User Page</h3> 
                {
-                   user["data"].map((item)=>{
-                       return(
+                   users["data"].map((item)=>{
+                    return(
                         <div key={item.id}>
                             <figure>
                                 <img src={item.avatar} alt=""/> 
-                            </figure>
+                            </figure>                         
+                        <p> first name:{item.first_name}  </p>                       
+                        <p> last name:{item.last_name}   </p> 
+                        <p> email:{item.email}   </p>      
+                        <p>===============================</p>                                                                          
                         </div>
-                       )                      
-                   })
+                    )                    
+                })
                }
         </div>
     )
